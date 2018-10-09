08d81ed5acc8d0995646cc7fefca99bbdee691be
SLRAM <stephier2290@gmail.com>
2018-10-09 16:23:14 -0400

    initial commit

---
+++ Snake/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Snake
+//
+//  Created by Stephanie Ramirez on 10/9/18.
+//  Copyright © 2018 Stephanie Ramirez. All rights reserved.
+//
+
+import Foundation
+
+//U+1F40D is snake
+
+let snake = "\u{1F40D}"
+for _ in 0...10 {
+    print(snake, terminator: "")
+}
